964859b6f5ec467c205e56a1012e6013e921c092
apovalyaevme <42703092+apovalyaevme@users.noreply.github.com>
2018-08-26 00:02:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const TelegramBot = require('node-telegram-bot-api');
 
 const token = '627663030:AAHA4Ccq4ZOUVw6HU0GDzwD4cTxo2uG02go';
 const bot = new TelegramBot(token, {polling: true});
-bot.onText(`message`, msg=> {
-    var userId = msg.from.id;
-    bot.sendMessage(userId, ' Не не, это Игорян - @stgiero');
+bot.onText('message', msg=> {
+    bot.sendMessage(msg.chat.id, '${msg.from.first_name}, Не не, это Игорян - @stgiero');
 })
