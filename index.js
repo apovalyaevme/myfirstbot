9d28995a0e984a9497515112fa2f0adff0dc3904
apovalyaevme <42703092+apovalyaevme@users.noreply.github.com>
2018-08-25 23:53:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-var TelegramBot = require('node-telegram-bot-api');
+const TelegramBot = require('node-telegram-bot-api');
 
-// Устанавливаем токен, который выдавал нам бот.
-var token = '627663030:AAHA4Ccq4ZOUVw6HU0GDzwD4cTxo2uG02go';
-// Включить опрос сервера
-var bot = new TelegramBot(token, {polling: true});
+
+const token = '627663030:AAHA4Ccq4ZOUVw6HU0GDzwD4cTxo2uG02go';
+const bot = new TelegramBot(token, {polling: true});
 bot.onText(`message`, msg=> {
     var userId = ;
-    bot.sendMessage(userId, ' Не не, пiдор однозначно @stgiero');
-});
+    bot.sendMessage(userId, ' ГЌГҐ Г­ГҐ, ГЇiГ¤Г®Г° Г®Г¤Г­Г®Г§Г­Г Г·Г­Г® @stgiero');
+})
